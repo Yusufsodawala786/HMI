8b4eb9dbe13a1ac9f96dd014c584b7790699dee0
Yusufsodawala786 <youzarsifsodawala7@gmail.com>
2024-04-16 00:07:26 +0530

    contact us page done

--- src/components/connect/connect.jsx
+++ src/components/connect/connect.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Map from './Map'
+import {Input} from "@nextui-org/react";
+import {Button, ButtonGroup} from "@nextui-org/react";
 
 const Connect = () => {
+
+  const handleSubmit = () => {
+    alert("Query has been submitted");
+    window.location.reload(); // Reloading the page
+  };
+
   return (
-    <div>
+    <div className=' hero-bg h-[87vh]'>
+        <span className='flex h-[20vh]  justify-center items-center font-bubblegum text-7xl'>Connect with us! </span><br/>
+        <div className='flex justify-between items-center gap-10 pr-24 '>
         <Map />
+        <div className='w-[50%] flex flex-col justify-center items-center bg-[#2e312f] rounded-xl mb-10'>
+        <span className='text-3xl font-bubblegum text-white pt-5'>Submit Your Query!</span>
+        <div  className="w-[100%] flex flex-col justify-center items-center  gap-4 p-10 ">
+          <Input type='text' label='Name' className='w-[70%]'/>
+          <Input type='text' label='Contact No' className='w-[70%]'/>
+          <Input type='text' label='Query' className='w-[70%]'/>
+          <Button color='primary' className='w-[30%] font-bubblegum text-xl' onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+        </div>
+        </div>
     </div>
   )
 }
